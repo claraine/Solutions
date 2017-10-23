0b2116baa73b32c59260df0e53348603f7677bcf
kharya0 <kkdvillegas@gmail.com>
2017-10-23 18:16:58 +0800

    Added AntonAndDanik() method to main class

--- Solutions/src/Solutions.java
+++ Solutions/src/Solutions.java
@@ -2,14 +2,42 @@
  * 
  *
  */
+import java.util.Scanner;
+import java.lang.*;
+
 public class Solutions {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
+	public static void AntonAndDanik() {
+		Scanner kbd = new Scanner(System.in);
+        int n = Integer.parseInt(kbd.next());
+        String s = kbd.next();
+        n = s.length();
+        int a = 0, d = 0;
+        
+        for (int x = 0; x < n; x++){
+        	if (s.charAt(x) == 'A'){
+        		a++;
+        	}
+        	else if (s.charAt(x) == 'D'){
+        		d++;
+        	}
+        }
+        
+        if (a > d){
+        	System.out.println("Anton");
+        }	
+        else if (d > a){
+        	System.out.println("Danik");
+        }
+        else{
+        	System.out.println("Friendship");
+        }
+	}
 }
